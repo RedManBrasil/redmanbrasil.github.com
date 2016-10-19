0dca2088e360fd40a894f76fcae89f8b1f3bacd6
RedManBrasil <pefandrade@hotmail.com>
2016-10-19 21:32:35 -0200

    78658

--- js/fbSDK.js
+++ js/fbSDK.js
@@ -1,9 +1,9 @@
 		// initialize and setup facebook js sdk
 		window.fbAsyncInit = function() {
 		    FB.init({
-		      appId      : 'APP_ID',
+		      appId      : '1866035216951680',
 		      xfbml      : true,
-		      version    : 'v2.5'
+		      version    : 'v2.8'
 		    });
 		    FB.getLoginStatus(function(response) {
 		    	if (response.status === 'connected') {
@@ -20,7 +20,7 @@
 		    var js, fjs = d.getElementsByTagName(s)[0];
 		    if (d.getElementById(id)) {return;}
 		    js = d.createElement(s); js.id = id;
-		    js.src = "//connect.facebook.net/en_US/sdk.js";
+		    js.src = "https://connect.facebook.net/en_US/sdk.js";
 		    fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));
 		
