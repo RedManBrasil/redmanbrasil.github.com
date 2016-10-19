fcc563e77aa7db5e50a567258ab82344c2f9ef88
RedManBrasil <pefandrade@hotmail.com>
2016-10-19 19:59:02 -0200

    979707

---
+++ js/fbSDK.js
@@ -0,0 +1,59 @@
+
+		// initialize and setup facebook js sdk
+		window.fbAsyncInit = function() {
+		    FB.init({
+		      appId      : '1866035216951680',
+		      xfbml      : true,
+		      version    : 'v2.8'
+		    });
+		    FB.getLoginStatus(function(response) {
+		    	if (response.status === 'connected') {
+		    		document.getElementById('status').innerHTML = 'We are connected.';
+		    		document.getElementById('login').style.visibility = 'hidden';
+		    	} else if (response.status === 'not_authorized') {
+		    		document.getElementById('status').innerHTML = 'We are not logged in.'
+		    	} else {
+		    		document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
+		    	}
+		    });
+		};
+		(function(d, s, id){
+		    var js, fjs = d.getElementsByTagName(s)[0];
+		    if (d.getElementById(id)) {return;}
+		    js = d.createElement(s); js.id = id;
+		    js.src = "https://connect.facebook.net/en_US/sdk.js";
+		    fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+		
+		// login with facebook with extra permissions
+		function login() {
+			FB.login(function(response) {
+				if (response.status === 'connected') {
+		    		document.getElementById('status').innerHTML = 'We are connected.';
+		    		document.getElementById('login').style.visibility = 'hidden';
+		    	} else if (response.status === 'not_authorized') {
+		    		document.getElementById('status').innerHTML = 'We are not logged in.'
+		    	} else {
+		    		document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
+		    	}
+			}, {scope: 'email'});
+		}
+
+		function getId() {
+			FB.api('/me', 'GET', {fields: 'id'}, function(response) {
+				document.getElementById('status').innerHTML = innerHTML + "\nSeu id é" + response.id;
+		}
+	}
+		
+		function getName() {
+			FB.api('/me', 'GET', {fields: 'name'}, function(response) {
+				document.getElementById('status').innerHTML = innerHTML + "Olá" + response.name + "!";
+		}
+	}
+
+		// getting basic user info
+		function getPhoto() {
+			FB.api('/me', 'GET', {fields: 'picture.width(150).height(150)'}, function(response) {
+				document.getElementById('status').innerHTML = innerHTML + "\n<img src='" + response.picture.data.url + "'>";
+			});
+		}
