490b136eebe7485a42cab07fd003839288eb0e98
RedManBrasil <pefandrade@hotmail.com>
2016-10-19 23:50:59 -0200

    6346

--- js/fbSDK.js
+++ js/fbSDK.js
@@ -30,6 +30,7 @@
 				if (response.status === 'connected') {
 		    		document.getElementById('status').innerHTML = 'We are connected.';
 		    		document.getElementById('login').style.visibility = 'hidden';
+		    		window.location.href = "2.html";
 		    	} else if (response.status === 'not_authorized') {
 		    		document.getElementById('status').innerHTML = 'We are not logged in.'
 		    	} else {
