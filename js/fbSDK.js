fcac92cf7df701cd04c3d8d68d56205725882472
RedManBrasil <pefandrade@hotmail.com>
2016-10-21 19:49:39 -0200

    738

--- js/fbSDK.js
+++ js/fbSDK.js
@@ -47,10 +47,10 @@
   			setTimeout(
 				function () {
 					FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id,picture.width(150).height(150)'}, function(response) {
-						document.getElementById('resposta').innerHTML = "Hello " + response.name + "!";
+						document.getElementById('resposta2').innerHTML = "Hello " + response.name + "!";
 		  				console.log(response);
 						});
-				} , 5000);
+				} , 200);
 		}
 
 		function getId() {
