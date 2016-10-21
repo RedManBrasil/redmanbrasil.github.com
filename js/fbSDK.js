160454d6f51288e2b69e2a6837fc010693dffd04
RedManBrasil <pefandrade@hotmail.com>
2016-10-21 20:29:24 -0200

    412678

--- js/fbSDK.js
+++ js/fbSDK.js
@@ -48,7 +48,6 @@
 				function () {
 					FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id,picture.width(150).height(150)'}, function(response) {
 						document.getElementById('resposta2').innerHTML = "Hello " + response.name + "!";
-		  				console.log(response);
 						});
 				} , 350);
 		}
