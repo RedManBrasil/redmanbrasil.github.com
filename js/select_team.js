71de63e268b9999c425a8614ca5968f516014eb5
RedManBrasil <pefandrade@hotmail.com>
2016-10-23 00:05:04 -0200

    658

--- js/select_team.js
+++ js/select_team.js
@@ -6,8 +6,29 @@ function check(){
 	else if(val=='Bot'){
 		document.getElementById('escudoTime').innerHTML = "<img src='escudos/Botafogo.png' style='float:center;''>";
 	}
-	else if(val=='millimeters'){
-		res = 3;
+	else if(val=='Vas'){
+		document.getElementById('escudoTime').innerHTML = "<img src='escudos/vasco.png' style='float:center;''>";
+	}
+	else if(val=='Flu'){
+		document.getElementById('escudoTime').innerHTML = "<img src='escudos/fluminense.png' style='float:center;''>";
+	}
+	else if(val=='Sp'){
+		document.getElementById('escudoTime').innerHTML = "<img src='escudos/Sao Paulo.png' style='float:center;''>";
+	}
+	else if(val=='San'){
+		document.getElementById('escudoTime').innerHTML = "<img src='escudos/Santos.png' style='float:center;''>";
+	}
+	else if(val=='Pal'){
+		document.getElementById('escudoTime').innerHTML = "<img src='escudos/palmeiras.png' style='float:center;''>";
+	}
+	else if(val=='Cor'){
+		document.getElementById('escudoTime').innerHTML = "<img src='escudos/Cornthias.png' style='float:center;''>";
+	}
+	else if(val=='AtlMG'){
+		document.getElementById('escudoTime').innerHTML = "<img src='escudos/Atletico Mineiro.png' style='float:center;''>";
+	}
+	else{
+		document.getElementById('escudoTime').innerHTML = "";
 	}
 
 }
