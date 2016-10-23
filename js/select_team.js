9d5bab1336f2fd42121c5991e502687162ea3e4f
RedManBrasil <pefandrade@hotmail.com>
2016-10-22 23:06:49 -0200

    568

--- js/select_team.js
+++ js/select_team.js
@@ -1,16 +1,13 @@
 function check(){
 	var val = document.getElementById('TeamBRList').value;
 	if(val=='Fla'){
-		document.getElementById('escudoTime').innerHTML = "<img src='escudos/Flamengo.png'>";
+		document.getElementById('escudoTime').innerHTML = "<img src='escudos/Flamengo.png' style='float:center;''>";
 	}
-	else if(val=='meters'){
-		res = 2;
+	else if(val=='Bot'){
+		document.getElementById('escudoTime').innerHTML = "<img src='escudos/Botafogo.png' style='float:center;''>";
 	}
 	else if(val=='millimeters'){
 		res = 3;
 	}
-	else{
-		res = 1;
-	}
-	return res;
+
 }
