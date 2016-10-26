43e5cfbfe4a185f0dd5298d98243705908f53aa8
RedManBrasil <pefandrade@hotmail.com>
2016-10-26 00:07:52 -0200

    658

---
+++ js/fbSDK.js
@@ -0,0 +1,79 @@
+		// initialize and setup facebook js sdk
+		window.fbAsyncInit = function() {
+		    FB.init({
+		      appId      : '1866035216951680',
+		      xfbml      : true,
+		      version    : 'v2.8'
+		    });
+		    FB.getLoginStatus(function(response) {
+		    	if (response.status === 'connected') {
+		    		document.getElementById('status').innerHTML = 'We are connected.';
+		    		document.getElementById('login').style.visibility = 'hidden';
+		    	} else if (response.status === 'not_authorized') {
+		    		document.getElementById('status').innerHTML = 'We are not logged in.'
+		    	} else {
+		    		document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
+		    	}
+		    });
+		};
+		(function(d, s, id){
+		    var js, fjs = d.getElementsByTagName(s)[0];
+		    if (d.getElementById(id)) {return;}
+		    js = d.createElement(s); js.id = id;
+		    js.src = "https://connect.facebook.net/en_US/sdk.js";
+		    fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+		
+		// login with facebook with extra permissions
+		function login() {
+			FB.login(function(response) {
+				if (response.status === 'connected') {
+		    		document.getElementById('status').innerHTML = 'We are connected.';
+		    		document.getElementById('login').style.visibility = 'hidden';
+		    		window.location.href = "first_time.html";
+					}
+		        else if (response.status === 'not_authorized') {
+		    		document.getElementById('status').innerHTML = 'We are not logged in.'
+		    	} else {
+		    		document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
+		    	}
+			}, {scope: 'email'});
+		}
+
+		
+		// getting basic user info
+		function getName() {
+  			setTimeout(
+				function () {
+					FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id,picture.width(150).height(150)'}, function(response) {
+						document.getElementById('resposta1').innerHTML = response.name;
+						});
+				} , 550);
+		}
+
+		function getPic() {
+			setTimeout(
+				function () {
+			FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id,picture.width(150).height(150)'}, function(response) {
+				document.getElementById('resposta').innerHTML = "<img src='" + response.picture.data.url + "' style='border-radius:50%;border:6px solid #0aa046;width:60%;'>";
+			});
+			} , 550);
+		}
+
+		function getId() {
+			setTimeout(
+				function () {
+			FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id,picture.width(150).height(150)'}, function(response) {
+				document.getElementById('resposta2').innerHTML = "Your Id is : " + response.id ;
+			});
+			} , 550);
+		}
+
+		function getName2() {
+  			setTimeout(
+				function () {
+					FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id,picture.width(150).height(150)'}, function(response) {
+						document.getElementById('resposta2').innerHTML = "Welcome " + response.name;
+						});
+				} , 550);
+  		}
