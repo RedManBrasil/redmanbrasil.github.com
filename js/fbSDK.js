91aa7e1ee7326e798d1dbac0bb6f701b53ffc294
RedManBrasil <pefandrade@hotmail.com>
2016-10-28 01:01:38 -0200

    435

--- js/fbSDK.js
+++ js/fbSDK.js
@@ -53,7 +53,7 @@
 			setTimeout(
 				function () {
 			FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id,picture.width(150).height(150)'}, function(response) {
-				document.getElementById('resposta').innerHTML = "<img src='" + response.picture.data.url + "' style='border-radius:50%;border:6px solid #0aa046;width:12%; margin-top: 20px; margin-left: 360px;'>";
+				document.getElementById('resposta').innerHTML = "<img src='" + response.picture.data.url + "' style='border-radius:50%;border:6px solid #0aa046;width:10%; margin-top: 20px; margin-left: 360px;'>";
 			});
 			} , 600);
 		}
