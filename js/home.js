d6ba4c7306efccc8c2b52332a7e80b10150d923f
RedManBrasil <pefandrade@hotmail.com>
2016-11-01 18:13:15 -0200

    534

--- js/home.js
+++ js/home.js
@@ -1,100 +1,55 @@
 $(function (){
+    var ChangeDivArray=new Array();     //array com as <div> onde irao conter as changes
+    var ChangeCoinArray=new Array();    //array com o valor de change das criptomoedas puxadas pelo API
+    var nameChangessList=new Array();   //array com o nome de cada time, associado a uma Change
+    var ChangessList=new Array();       //array com a ordem da maior change diaria para a menor
 
-    var $firstChange = $('#firstChange');
-    var $secondChange = $('#secondChange');
-    var $thirdChange = $('#thirdChange');
-    var $fourthChange = $('#fourthChange');
-    var $fifthChange = $('#fifthChange');
+    for (var n = 0; n != 10; ++n){ //o numero apos o != representa o numero de diferentes divs
+     ChangeDivArray[n] = $('#changediv'+n);} //onde puxa a id de cada <div> que sera modificado
+
     $.ajax({
         type: 'GET',
-        url: "https://api.coinmarketcap.com/v1/ticker/?limit=5",
-        success: function(firstChange) {
-            $.each(firstChange, function(i, change) {
+        url: "https://api.coinmarketcap.com/v1/ticker/?limit=10",
+        success: function(response) {
+            $.each(response, function(i, change) {
 
-                if(i==0){
-                    changebtc = change.percent_change_24h;
-                    console.log(changebtc);
-                }
-                else if(i==1){
-                    changeeth = change.percent_change_24h;
-                    console.log(changeeth);
-                }
-                else if(i==2){
-                    changerip = change.percent_change_24h;
-                    console.log(changerip);
-                }
-                else if(i==3){
-                    changelit = change.percent_change_24h;
-                    console.log(changelit);
-                }
-                else if(i==4){
-                    changeetc = change.percent_change_24h;
+                if(i==0){ChangeCoinArray[i] = change.percent_change_24h; ChangessList.push(ChangeCoinArray[i])}
+                else if(i==1){ChangeCoinArray[i] = change.percent_change_24h;ChangessList.push(ChangeCoinArray[i])}
+                else if(i==2){ChangeCoinArray[i] = change.percent_change_24h;ChangessList.push(ChangeCoinArray[i])}
+                else if(i==3){ChangeCoinArray[i] = change.percent_change_24h;ChangessList.push(ChangeCoinArray[i])}
+                else if(i==4){ChangeCoinArray[i] = change.percent_change_24h;ChangessList.push(ChangeCoinArray[i])}
+                else if(i==5){ChangeCoinArray[i] = change.percent_change_24h;ChangessList.push(ChangeCoinArray[i])}
+                else if(i==6){ChangeCoinArray[i] = change.percent_change_24h;ChangessList.push(ChangeCoinArray[i])}
+                else if(i==7){ChangeCoinArray[i] = change.percent_change_24h;ChangessList.push(ChangeCoinArray[i])}
+                else if(i==8){ChangeCoinArray[i] = change.percent_change_24h;ChangessList.push(ChangeCoinArray[i])}
+                else if(i==9){ChangeCoinArray[i] = change.percent_change_24h;ChangessList.push(ChangeCoinArray[i])
                     function sortNumber(a,b) {
                     return b - a;
                             }
-
-                    ChangessList = [changebtc, changeeth, changerip, changelit, changeetc];
-                    ChangessList.sort(sortNumber);
-                    console.log(ChangessList);
-                    change0 = ChangessList[0];
-                    change1 = ChangessList[1];
-                    change2 = ChangessList[2];
-                    change3 = ChangessList[3];
-                    change4 = ChangessList[4];
-
-                    var nameChangessList = [];
-                    for (var n = 0; n != 5; ++n) {
-                                if (ChangessList[n] == changebtc){nameChangessList[n] = "   Flamengo"}
-                                if (ChangessList[n] == changeetc){nameChangessList[n] = "   Santos"}
-                                if (ChangessList[n] == changeeth){nameChangessList[n] = "   Internacional"}
-                                if (ChangessList[n] == changelit){nameChangessList[n] = "   China"}
-                                if (ChangessList[n] == changerip){nameChangessList[n] = "   Brazil"}
+                    ChangessList.sort(sortNumber); // coloca os valores na ordem crescente/decrescente chamando a funcao SortNumber
+                    for (var n = 0; n != 10; ++n) { //o numero apos o != representa o numero de diferentes times
+                                if (ChangessList[n] == ChangeCoinArray[0]){nameChangessList[n] = "   Flamengo"}
+                                if (ChangessList[n] == ChangeCoinArray[1]){nameChangessList[n] = "   Santos"}
+                                if (ChangessList[n] == ChangeCoinArray[2]){nameChangessList[n] = "   Internacional"}
+                                if (ChangessList[n] == ChangeCoinArray[3]){nameChangessList[n] = "   China"}
+                                if (ChangessList[n] == ChangeCoinArray[4]){nameChangessList[n] = "   Brazil"}
+                                if (ChangessList[n] == ChangeCoinArray[5]){nameChangessList[n] = "   Chapecoense"}
+                                if (ChangessList[n] == ChangeCoinArray[6]){nameChangessList[n] = "   Botafogo"}
+                                if (ChangessList[n] == ChangeCoinArray[7]){nameChangessList[n] = "   USA"}
+                                if (ChangessList[n] == ChangeCoinArray[8]){nameChangessList[n] = "   Palmeiras"}
+                                if (ChangessList[n] == ChangeCoinArray[9]){nameChangessList[n] = "   Corinthias"}
                             }
-                    console.log(nameChangessList);
 
-                
-                if(change0 > 0){
-                    $firstChange.append('&nbsp;&nbsp;&nbsp;&nbsp;<img src="css/imgs/UpGreen.png" style="margin-bottom:-2px;width: 6%;">&nbsp;&nbsp;<h2 style="display:inline-block;color:#0aa046;">( '+ change0 +'%)'+ nameChangessList[0] + '</h2><br>');}
-                else if (change0 < 0){
-                    $firstChange.append('&nbsp;&nbsp;&nbsp;&nbsp;<img src="css/imgs/DownRed.png" style="margin-bottom:-2px;width: 6%;">&nbsp;&nbsp;<h2 style="display:inline-block;color:#dd5056;">( '+ change0 +'%)'+ nameChangessList[0] + '</h2><br>');}
-                else{
-                    $firstChange.append('&nbsp;&nbsp;&nbsp;&nbsp;<img src="css/imgs/EqualGray.png" style="margin-bottom:-2px;width: 6%;">&nbsp;&nbsp;<h2 style="display:inline-block;color:#9a9090;">( '+ change0 +'%)'+ nameChangessList[0] + '</h2><br>');}
-                
-                            
-                    if(change1 > 0){
-                    $secondChange.append('&nbsp;&nbsp;&nbsp;&nbsp;<img src="css/imgs/UpGreen.png" style="margin-bottom:-2px;width: 6%;">&nbsp;&nbsp;<h2 style="display:inline-block;color:#0aa046;">( '+ change1 +'%)'+ nameChangessList[1] + '</h2><br>');}
-                else if (change1 < 0){
-                    $secondChange.append('&nbsp;&nbsp;&nbsp;&nbsp;<img src="css/imgs/DownRed.png" style="margin-bottom:-2px;width: 6%;">&nbsp;&nbsp;<h2 style="display:inline-block;color:#dd5056;">( '+ change1 +'%)'+ nameChangessList[1] + '</h2><br>');}
-                else{
-                    $secondChange.append('&nbsp;&nbsp;&nbsp;&nbsp;<img src="css/imgs/EqualGray.png" style="margin-bottom:-2px;width: 6%;">&nbsp;&nbsp;<h2 style="display:inline-block;color:#9a9090;">( '+ change1 +'%)'+ nameChangessList[1] + '</h2><br>');}
-                
-
-                    if(change2 > 0){
-                    $thirdChange.append('&nbsp;&nbsp;&nbsp;&nbsp;<img src="css/imgs/UpGreen.png" style="margin-bottom:-2px;width: 6%;">&nbsp;&nbsp;<h2 style="display:inline-block;color:#0aa046;">( '+ change2 +'%)'+ nameChangessList[2] + '</h2><br>');}
-                else if (change2 < 0){
-                    $thirdChange.append('&nbsp;&nbsp;&nbsp;&nbsp;<img src="css/imgs/DownRed.png" style="margin-bottom:-2px;width: 6%;">&nbsp;&nbsp;<h2 style="display:inline-block;color:#dd5056;">( '+ change2 +'%)'+ nameChangessList[2] + '</h2><br>');}
-                else{
-                    $thirdChange.append('&nbsp;&nbsp;&nbsp;&nbsp;<img src="css/imgs/EqualGray.png" style="margin-bottom:-2px;width: 6%;">&nbsp;&nbsp;<h2 style="display:inline-block;color:#9a9090;">( '+ change2 +'%)'+ nameChangessList[2] + '</h2><br>');}
-
-                
-                    if(change3 > 0){
-                    $fourthChange.append('&nbsp;&nbsp;&nbsp;&nbsp;<img src="css/imgs/UpGreen.png" style="margin-bottom:-2px;width: 6%;">&nbsp;&nbsp;<h2 style="display:inline-block;color:#0aa046;">( '+ change3 +'%)'+ nameChangessList[3] + '</h2><br>');}
-                else if (change3 < 0){
-                    $fourthChange.append('&nbsp;&nbsp;&nbsp;&nbsp;<img src="css/imgs/DownRed.png" style="margin-bottom:-2px;width: 6%;">&nbsp;&nbsp;<h2 style="display:inline-block;color:#dd5056;">( '+ change3 +'%)'+ nameChangessList[3] + '</h2><br>');}
-                else{
-                    $fourthChange.append('&nbsp;&nbsp;&nbsp;&nbsp;<img src="css/imgs/EqualGray.png" style="margin-bottom:-2px;width: 6%;">&nbsp;&nbsp;<h2 style="display:inline-block;color:#9a9090;">( '+ change3 +'%)'+ nameChangessList[3] + '</h2><br>');}
-                
-       
-                    if(change4 > 0){
-                    $fifthChange.append('&nbsp;&nbsp;&nbsp;&nbsp;<img src="css/imgs/UpGreen.png" style="margin-bottom:-2px;width: 6%;">&nbsp;&nbsp;<h2 style="display:inline-block;color:#0aa046;">( '+ change4 +'%)'+ nameChangessList[4] + '</h2><br>');}
-                else if (change4 < 0){
-                    $fifthChange.append('&nbsp;&nbsp;&nbsp;&nbsp;<img src="css/imgs/DownRed.png" style="margin-bottom:-2px;width: 6%;">&nbsp;&nbsp;<h2 style="display:inline-block;color:#dd5056;">( '+ change4 +'%)'+ nameChangessList[4] + '</h2><br>');}
-                else{
-                    $fifthChange.append('&nbsp;&nbsp;&nbsp;&nbsp;<img src="css/imgs/EqualGray.png" style="margin-bottom:-2px;width: 6%;">&nbsp;&nbsp;<h2 style="display:inline-block;color:#9a9090;">( '+ change4 +'%)'+ nameChangessList[4] + '</h2><br>');}
+                  for (var n = 0; n != 10; ++n) { //o numero apos o != representa o numero de diferentes times
+                if(ChangessList[n] > 0){ //maior que zero é verde
+                    ChangeDivArray[n].append('&nbsp;&nbsp;&nbsp;&nbsp;<img src="css/imgs/UpGreen.png" style="margin-bottom:-2px;width: 6%;">&nbsp;&nbsp;<h2 style="display:inline-block;color:#0aa046;">( '+ ChangessList[n] +'%)'+ nameChangessList[n] + '</h2><br>');}
+                else if (ChangessList[n] < 0){ //menor que zero é vermelho
+                    ChangeDivArray[n].append('&nbsp;&nbsp;&nbsp;&nbsp;<img src="css/imgs/DownRed.png" style="margin-bottom:-2px;width: 6%;">&nbsp;&nbsp;<h2 style="display:inline-block;color:#dd5056;">( '+ ChangessList[n] +'%)'+ nameChangessList[n] + '</h2><br>');}
+                else{ //igual a zero é cinza
+                    ChangeDivArray[n].append('&nbsp;&nbsp;&nbsp;&nbsp;<img src="css/imgs/EqualGray.png" style="margin-bottom:-2px;width: 6%;">&nbsp;&nbsp;<h2 style="display:inline-block;color:#9a9090;">( '+ ChangessList[n] +'%)'+ nameChangessList[n] + '</h2><br>');}
+                }    
          }
-
             });
             }
-
         });
 });
