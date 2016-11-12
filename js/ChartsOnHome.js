961b2ac334208a12a2531755172090bf07da4a0e
RedManBrasil <pefandrade@hotmail.com>
2016-11-12 00:54:54 -0200

    547

--- js/ChartsOnHome.js
+++ js/ChartsOnHome.js
@@ -5,105 +5,8 @@
       function getRandomArbitrary(min, max) { //gera numero aleatorio
             return Math.random() * (max - min) + min;
         }
-      Chart.defaults.global.tooltips.custom = function(tooltip) {
 
-      // Tooltip Element
-      var tooltipEl = $('#chartjs-tooltip');
-
-      // Hide if no tooltip
-      if (!tooltip.opacity) {
-        tooltipEl.css({
-          opacity: 0
-        });
-        $('.chartjs-wrap canvas')
-          .each(function(index, el) {
-            $(el).css('cursor', 'default');
-          });
-        return;
-      }
-
-      $(this._chart.canvas).css('cursor', 'pointer');
-
-      // Set caret Position
-      tooltipEl.removeClass('above below no-transform');
-      if (tooltip.yAlign) {
-        tooltipEl.addClass(tooltip.yAlign);
-      } else {
-        tooltipEl.addClass('no-transform');
-      }
-
-      // Set Text
-      if (tooltip.body) {
-        var innerHtml = [
-          (tooltip.beforeTitle || []).join('\n'), (tooltip.title || []).join('\n'), (tooltip.afterTitle || []).join('\n'), (tooltip.beforeBody || []).join('\n'), (tooltip.body || []).join('\n'), (tooltip.afterBody || []).join('\n'), (tooltip.beforeFooter || [])
-          .join('\n'), (tooltip.footer || []).join('\n'), (tooltip.afterFooter || []).join('\n')
-        ];
-        tooltipEl.html(innerHtml.join('\n'));
-      }
-
-      // Find Y Location on page
-      var top = 0;
-      if (tooltip.yAlign) {
-        if (tooltip.yAlign == 'above') {
-          top = tooltip.y - tooltip.caretHeight - tooltip.caretPadding;
-        } else {
-          top = tooltip.y + tooltip.caretHeight + tooltip.caretPadding;
-        }
-      }
-
-      var position = $(this._chart.canvas)[0].getBoundingClientRect();
-
-      // Display, position, and set styles for font
-      tooltipEl.css({
-        opacity: 1,
-        width: tooltip.width ? (tooltip.width + 'px') : 'auto',
-        left: position.left + tooltip.x + 'px',
-        top: position.top + top + 'px',
-        fontFamily: tooltip._fontFamily,
-        fontSize: tooltip.fontSize,
-        fontStyle: tooltip._fontStyle,
-        padding: tooltip.yPadding + 'px ' + tooltip.xPadding + 'px',
-      });
-    };
-
-    var ChartData=new Array();  
-    for (var n = 0; n != 6; ++n){ 
-   ChartData[n] = getRandomInt(0, 300);}
-    var config = {
-        type: 'pie',
-        data: {
-            datasets: [{
-                data: [ChartData[0], ChartData[1], ChartData[2], ChartData[3], ChartData[4]],
-                backgroundColor: [
-                    "#F7464A",
-                    "#62bb46",
-                    "#355DDE",
-                    "#ffd400",
-                    "#855555",
-                ],
-            }],
-            labels: [
-                "Flamengo",
-                "Palmeiras",
-                "Cruzeiro",
-                "Brazil",
-                "Vitória"
-            ]
-        },
-        options: {
-            responsive: true,
-            legend: {
-                display: false
-            },
-            tooltips: {
-                enabled: true,
-            }
-        }
-    };
-
-//SEPARATION CHART 1 to CHART 2
-
-        var datapointsNetWorth = [ChartData[0], ChartData[1], ChartData[2], ChartData[3], ChartData[4], ChartData[5]];
+    var datapointsNetWorth = [getRandomInt(0, 300), getRandomInt(0, 300), getRandomInt(0, 300), getRandomInt(0, 300),getRandomInt(0, 300), getRandomInt(0, 300)];
     var configNetWorth = {
             type: 'line',
             data: {
@@ -119,9 +22,12 @@
             },
             options: {
                 responsive: true,
+                maintainAspectRatio: false,
                 title:{
                     display:false,
-                    text:'Chart.js Line Chart - Cubic interpolation mode'
+                },
+                legend: {
+                display: false,
                 },
                 tooltips: {
                     mode: 'label'
@@ -150,4 +56,56 @@
                 }
             }
         };
+
+       //SEPARATION CHART 1 to CHART 2
+
+    var config = {
+        type: 'doughnut',
+        data: {
+            datasets: [{
+                data: [
+                    getRandomInt(0, 300),
+                    getRandomInt(0, 300),
+                    getRandomInt(0, 300),
+                    getRandomInt(0, 300),
+                    getRandomInt(0, 300),
+                ],
+                backgroundColor: [
+                    "#F7464A",
+                    "#62bb46",
+                    "#355DDE",
+                    "#ffd400",
+                    "#855555",
+                ],
+            },
+             ],
+            labels: [
+                "Flamengo",
+                "Palmeiras",
+                "Cruzeiro",
+                "Brazil",
+                "Vitória"
+            ]
+        },
+        options: {
+            responsive: true,
+            legend: {
+                display: false,
+            },
+            title: {
+                display: false,
+                text: 'My Portfolio'
+            },
+            animation: {
+                animateScale: true,
+                animateRotate: true
+            }
+        }
+    };
+
+    window.onload = function() {
+        var ctx = document.getElementById("chart-area").getContext("2d");
+        window.myDoughnut = new Chart(ctx, config);
+    };
+
 
