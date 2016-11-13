02e4e6ff4b27fa303a85471ca69f473275cf2964
RedManBrasil <pefandrade@hotmail.com>
2016-11-13 01:08:04 -0200

    425

--- js/ChartsOnHome.js
+++ js/ChartsOnHome.js
@@ -66,12 +66,12 @@
         //SEPARATION CHART 1(NetWorthSemester) to CHART 2(NetWorthWeek)
 
             var datapointsNetWorthWeek = [getRandomInt(0, 300),
-                                       getRandomInt(0, 300),
-                                       getRandomInt(0, 300),
-                                       getRandomInt(0, 300),
-                                       getRandomInt(0, 300),
-                                       getRandomInt(0, 300),
-                                       getRandomInt(0, 300)];
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300)];
             var configNetWorthWeek = {
             type: 'line',
             data: {
@@ -121,9 +121,143 @@
                 }
             }
         };
+        //SEPARATION CHART 2(NetWorthWeek) to CHART 3(NetWorthToday)
+
+            var datapointsNetWorthToday = [getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300)];
+            var configNetWorthToday = {
+            type: 'line',
+            data: {
+                labels: ["00:00", "04:00", "08:00", "12;00", "16:00", "20:00", "24:00"],
+                datasets: [{
+                    label: "Net Worth",
+                    data: datapointsNetWorthToday,
+                    borderColor: 'rgba(255, 0, 0, 0.7)',
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+                    fill: false,
+          cubicInterpolationMode: 'monotone'
+                },  ]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                title:{
+                    display:false,
+                },
+                legend: {
+                display: false,
+                },
+                tooltips: {
+                    mode: 'label'
+                },
+                hover: {
+                    mode: 'dataset'
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true
+                        }
+                    }],
+                    yAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Value(Dollars)'
+                        },
+                        ticks: {
+                            suggestedMin: 0,
+                            suggestedMax: 300,
+                        }
+                    }]
+                }
+            }
+        };
 
 
-       //SEPARATION CHART 2(NetWorthWeek) to CHART 3(Donut)
+//SEPARATION CHART 3(NetWorthToday) to CHART 4(NetWorthYear)
+
+            var datapointsNetWorthYear = [getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300),
+                                          getRandomInt(0, 300)];
+            var configNetWorthYear = {
+            type: 'line',
+            data: {
+                labels: ["January",
+                         "February",
+                          "March",
+                          "April",
+                           "May",
+                           "June",
+                           "July",
+                           "August",
+                           "September",
+                           "October",
+                           "November",
+                           "December"],
+                datasets: [{
+                    label: "Net Worth",
+                    data: datapointsNetWorthYear,
+                    borderColor: 'rgba(255, 0, 0, 0.7)',
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+                    fill: false,
+          cubicInterpolationMode: 'monotone'
+                },  ]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                title:{
+                    display:false,
+                },
+                legend: {
+                display: false,
+                },
+                tooltips: {
+                    mode: 'label'
+                },
+                hover: {
+                    mode: 'dataset'
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true
+                        }
+                    }],
+                    yAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Value(Dollars)'
+                        },
+                        ticks: {
+                            suggestedMin: 0,
+                            suggestedMax: 300,
+                        }
+                    }]
+                }
+            }
+        };
+
+
+       //SEPARATION CHART 4(NetWorthYear) to CHART 5(Donut)
 
     var config = {
         type: 'doughnut',
