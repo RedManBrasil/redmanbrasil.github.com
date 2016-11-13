9fff610082a2ad6788eb4d955f8267c115147432
RedManBrasil <pefandrade@hotmail.com>
2016-11-12 23:51:11 -0200

    425

--- js/ChartsOnHome.js
+++ js/ChartsOnHome.js
@@ -6,11 +6,17 @@
             return Math.random() * (max - min) + min;
         }
 
-    var datapointsNetWorth = [getRandomInt(0, 300), getRandomInt(0, 300), getRandomInt(0, 300), getRandomInt(0, 300),getRandomInt(0, 300), getRandomInt(0, 300)];
+    var datapointsNetWorth = [getRandomInt(0, 300),
+                              getRandomInt(0, 300),
+                              getRandomInt(0, 300),
+                              getRandomInt(0, 300),
+                              getRandomInt(0, 300),
+                              getRandomInt(0, 300),
+                              getRandomInt(0, 300)];
     var configNetWorth = {
             type: 'line',
             data: {
-                labels: ["January", "February", "March", "April", "May", "June"],
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
                 datasets: [{
                     label: "Net Worth",
                     data: datapointsNetWorth,
@@ -57,7 +63,67 @@
             }
         };
 
-       //SEPARATION CHART 1 to CHART 2
+        //SEPARATION CHART 1(NetWorth) to CHART 2(NetWorth2)
+
+            var datapointsNetWorth2 = [getRandomInt(0, 300),
+                                       getRandomInt(0, 300),
+                                       getRandomInt(0, 300),
+                                       getRandomInt(0, 300),
+                                       getRandomInt(0, 300),
+                                       getRandomInt(0, 300),
+                                       getRandomInt(0, 300)];
+            var configNetWorth2 = {
+            type: 'line',
+            data: {
+                labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+                datasets: [{
+                    label: "Net Worth",
+                    data: datapointsNetWorth2,
+                    borderColor: 'rgba(255, 0, 0, 0.7)',
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+                    fill: false,
+          cubicInterpolationMode: 'monotone'
+                },  ]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                title:{
+                    display:false,
+                },
+                legend: {
+                display: false,
+                },
+                tooltips: {
+                    mode: 'label'
+                },
+                hover: {
+                    mode: 'dataset'
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true
+                        }
+                    }],
+                    yAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Value(Dollars)'
+                        },
+                        ticks: {
+                            suggestedMin: 0,
+                            suggestedMax: 300,
+                        }
+                    }]
+                }
+            }
+        };
+
+
+       //SEPARATION CHART 2(NetWorth2) to CHART 3(Donut)
 
     var config = {
         type: 'doughnut',
