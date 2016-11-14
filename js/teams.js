2fdbfa1a9fec68e936e3214725063e51e8b59df4
RedManBrasil <pefandrade@hotmail.com>
2016-11-14 16:57:28 -0200

    3214

--- js/teams.js
+++ js/teams.js
@@ -1,4 +1,5 @@
-var teamsBRDivContent = "<table>" +
+var teamsBRDivContent =
+			 "<table>" +
 				"<tr>" +
 					"<th>/</th>" +
 					"<th>Name</th>" +
@@ -19,5 +20,26 @@ var teamsBRDivContent = "<table>" +
 					"<td>4.9Bi</td>" +
 					"<td>$19.84</td>" +
 					"<td>-3%</td>" +
+				"</tr>" +
+				"<tr>" +
+					"<td><img src='escudos/brazilian_league/escudos_mini/botafogo_mini.png'></td>" +
+					"<td>Botafogo</td>"+
+					"<td>4.2Bi</td>" +
+					"<td>$17.93</td>" +
+					"<td>8%</td>" +
+				"</tr>" +
+				"<tr>" +
+					"<td><img src='escudos/brazilian_league/escudos_mini/vasco_mini.png'></td>" +
+					"<td>Vasco</td>"+
+					"<td>3.1Bi</td>" +
+					"<td>$12.45</td>" +
+					"<td>-16%</td>" +
+				"</tr>" +
+				"<tr>" +
+					"<td><img src='escudos/brazilian_league/escudos_mini/fluminense_mini.png'></td>" +
+					"<td>Fluminense</td>"+
+					"<td>3.6Bi</td>" +
+					"<td>$15.93</td>" +
+					"<td>-1%</td>" +
 				"</tr>" +
 			"</table>";
