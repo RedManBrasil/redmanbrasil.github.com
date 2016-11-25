a33a16ac3de853c42ded88344a0c52194eb9b164
RedManBrasil <pefandrade@hotmail.com>
2016-11-24 22:16:57 -0200

    5436

---
+++ js/RankingsAPItest.js
@@ -0,0 +1,26 @@
+var RankingsAPI = [
+	{
+		"id": "0",
+		"name": "Pedro Andrade",
+		"countrycode": "BR",
+		"networth": "142241.23",
+		"monthchange": "35.2",
+		"firmname": "First Things First"
+		},
+	{
+		"id": "1",
+		"name": "Lucas Varella",
+		"countrycode": "US",
+		"networth": "110135.75",
+		"monthchange": "19.1",
+		"firmname": "-"
+		},
+	{
+		"id": "2",
+		"name": "Tiago Simões",
+		"countrycode": "GR",
+		"networth": "83145.51",
+		"monthchange": "7.8",
+		"firmname": "Corporis Inc."
+		}
+]
---
+++ js/rankingsContent.js
@@ -0,0 +1,34 @@
+
+function CreateTableRankingsAPI(){
+var headerRankDivContent = //header para conter o início da tabela que só aparece uma vez, por isso não entra no for
+			 "<table id='" + "Rankings" + "TableContent'>" +
+			  "<thead>" +
+				"<tr>" +
+					"<th>/</th>" +
+					"<th>Name</th>" +
+					"<th>Monthly Change</th>" +
+					"<th>Net Worth</th>" +
+					"<th>Firm</th>" +
+				"</tr>" +
+			  "</thead>";
+
+var bodiesRankDivContent = ""; //necessario para nao aparecer undefined com o i=0, pois a variavel ainda estara vazia
+
+	for(var i = 0; (RankingsAPI.length) > i; i++){ //for que vai criando a tabela para cada time da array
+	bodiesRankDivContent = bodiesRankDivContent + //o conteudo da array RankingsAPI esta em .../js/RankingsAPItest.js
+				"<tr id='" + RankingsAPI[i]["id"] + "RankingsTableTD'>" +
+					"<td><img src='escudos/escudos_mini/" + RankingsAPI[i]["countrycode"] + "_mini.png'></td>" +
+					"<td>" + RankingsAPI[i]["name"] + "</td>"+
+					"<td>" + RankingsAPI[i]["monthchange"] + "%</td>" +
+					"<td>" + RankingsAPI[i]["networth"] + " dollars</td>" +
+					"<td>" + RankingsAPI[i]["firmname"] + "</td>" +
+				"</tr>";
+	}
+var fullRankDivContent = headerRankDivContent +
+			"<tbody>" +
+ 				 bodiesRankDivContent +
+				"</tbody>" +
+			"</table>";
+
+return fullRankDivContent;
+}
