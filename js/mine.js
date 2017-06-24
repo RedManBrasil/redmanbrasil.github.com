7751fc34b86d290b32e8940eca99f04e6ed40c95
RedManBrasil <pefandrade@hotmail.com>
2017-06-24 17:52:47 -0300

    453543

--- js/mine.js
+++ js/mine.js
@@ -40,7 +40,7 @@ $(function (){
 function continueExecution(){ //essa função só será chamada quando o primiero AJAX acabar
      $.ajax({
         type: 'GET',
-        url: "https://api.coinmarketcap.com/v1/ticker/?limit=150",
+        url: "https://api.coinmarketcap.com/v1/ticker/?limit=800",
         success: function(response) {
             $.each(response, function(i, change) {
                 if ( change.id == 'bitcoin' ){
@@ -106,6 +106,10 @@ function continueExecution(){ //essa função só será chamada quando o primier
                 CoinsChange[change.id] = change.percent_change_24h;
                 showNumbers(change.percent_change_24h, change.id, change.price_btc);
                 }
+                else if ( change.id == 'bancor-network'){
+                CoinsChange[change.id] = change.percent_change_24h;
+                showNumbers(change.percent_change_24h, change.id, change.price_btc);
+                }
             });
         }
     });
@@ -167,7 +171,7 @@ function showNumbers(a, b, c) {
 
 $(document).ajaxComplete(function(event,xhr,settings){
     console.log("URL",settings.url);
-    if(settings.url === "https://api.coinmarketcap.com/v1/ticker/?limit=150")
+    if(settings.url === "https://api.coinmarketcap.com/v1/ticker/?limit=800")
     { //faz a soma do valor em btc dos portfolios, para o total
         var tot_btc_geral = 0; //valor em btc somando todos os users
         var tot_real_geral = 0; //valor em reais somando todos os users
