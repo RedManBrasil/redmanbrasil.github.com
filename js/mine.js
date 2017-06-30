51e17ef12a64343378e71787df9b4bb93411ce9d
RedManBrasil <pefandrade@hotmail.com>
2017-06-30 09:51:06 -0300

    425

--- js/mine.js
+++ js/mine.js
@@ -114,6 +114,10 @@ function continueExecution(){ //essa função só será chamada quando o primier
                 CoinsChange[change.id] = change.percent_change_24h;
                 showNumbers(change.percent_change_24h, change.id, change.price_btc);
                 }
+                else if ( change.id == 'status'){
+                CoinsChange[change.id] = change.percent_change_24h;
+                showNumbers(change.percent_change_24h, change.id, change.price_btc);
+                }
             });
         }
     });
