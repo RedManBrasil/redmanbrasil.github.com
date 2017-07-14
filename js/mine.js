8c798cb6f815923a0e93a08a9afd3ce39b217e50
RedManBrasil <pefandrade@hotmail.com>
2017-07-14 17:30:54 -0300

    4435

--- js/mine.js
+++ js/mine.js
@@ -40,7 +40,7 @@ $(function (){
 function continueExecution(){ //essa função só será chamada quando o primiero AJAX acabar
      $.ajax({
         type: 'GET',
-        url: "https://api.coinmarketcap.com/v1/ticker/?limit=150",
+        url: "https://api.coinmarketcap.com/v1/ticker/?limit=250",
         success: function(response) {
             $.each(response, function(i, change) {
                 if ( change.id == 'bitcoin' ){
@@ -179,7 +179,7 @@ function showNumbers(a, b, c) {
 
 $(document).ajaxComplete(function(event,xhr,settings){
     console.log("URL",settings.url);
-    if(settings.url === "https://api.coinmarketcap.com/v1/ticker/?limit=150")
+    if(settings.url === "https://api.coinmarketcap.com/v1/ticker/?limit=250")
     { //faz a soma do valor em btc dos portfolios, para o total
         var tot_btc_geral = 0; //valor em btc somando todos os users
         var tot_real_geral = 0; //valor em reais somando todos os users
