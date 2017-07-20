ea0dd2bca0c2c3c7e128d277db331a820c461675
Pedro Enrique Andrade <pefandrade@hotmail.com>
2017-07-20 11:54:45 -0300

    534t54372

--- js/mine.js
+++ js/mine.js
@@ -82,7 +82,7 @@ function continueExecution(){ //essa função só será chamada quando o primier
                 CoinsChange[change.id] = change.percent_change_24h;
                 showNumbers(change.percent_change_24h, change.id, change.price_btc);
                 }
-                else if ( change.id == 'STORJ' ){
+                else if ( change.id == 'storj' ){
                 CoinsChange[change.id] = change.percent_change_24h;
                 showNumbers(change.percent_change_24h, change.id, change.price_btc);
                 }
