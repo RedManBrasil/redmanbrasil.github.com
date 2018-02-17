d16fc6b4353d194a8b8040189d42f651ebbc54c8
Pedro Enrique Andrade <pefandrade@hotmail.com>
2018-02-17 09:10:13 -0200

    268

--- js/mine.js
+++ js/mine.js
@@ -41,7 +41,7 @@ $(function (){
 function continueExecution(){ //essa função só será chamada quando o primiero AJAX acabar
      $.ajax({
         type: 'GET',
-        url: "https://api.coinmarketcap.com/v1/ticker/?limit=625",
+        url: "https://api.coinmarketcap.com/v1/ticker/?limit=650",
         success: function(response) {
             $.each(response, function(i, change) {
                 if ( change.id == 'bitcoin' ){
@@ -216,7 +216,7 @@ function showNumbers(a, b, c) {
 
 $(document).ajaxComplete(function(event,xhr,settings){
     console.log("URL",settings.url);
-    if(settings.url === "https://api.coinmarketcap.com/v1/ticker/?limit=625")
+    if(settings.url === "https://api.coinmarketcap.com/v1/ticker/?limit=650")
     { //faz a soma do valor em btc dos portfolios, para o total
         var tot_btc_geral = 0; //valor em btc somando todos os users
         var tot_real_geral = 0; //valor em reais somando todos os users
