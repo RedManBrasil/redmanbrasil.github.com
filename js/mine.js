68b154e0f6e2da37a26a61ec30894e2f7adfbcb2
Pedro Enrique Andrade <pefandrade@hotmail.com>
2018-12-03 01:05:28 -0200

    2424

--- js/mine.js
+++ js/mine.js
@@ -41,7 +41,7 @@ $(function (){
 function continueExecution(){ //essa função só será chamada quando o primiero AJAX acabar
      $.ajax({
         type: 'GET',
-        url: "https://api.coinmarketcap.com/v1/ticker/?limit=1300",
+        url: "https://api.coinmarketcap.com/v1/ticker/?limit=1800",
         success: function(response) {
             $.each(response, function(i, change) {
                 if ( change.id == 'bitcoin' ){
@@ -139,7 +139,7 @@ function continueExecution(){ //essa função só será chamada quando o primier
                 CoinsChange[change.id] = change.percent_change_24h;
                 showNumbers(change.percent_change_24h, change.id, change.price_btc);
                 }
-                else if ( change.id == 'creativecoin' ){
+                else if ( change.id == 'crea' ){
                 CoinsChange[change.id] = change.percent_change_24h;
                 showNumbers(change.percent_change_24h, change.id, change.price_btc);
                 }
@@ -208,7 +208,7 @@ function showNumbers(a, b, c) {
 
 $(document).ajaxComplete(function(event,xhr,settings){
     console.log("URL",settings.url);
-    if(settings.url === "https://api.coinmarketcap.com/v1/ticker/?limit=1300")
+    if(settings.url === "https://api.coinmarketcap.com/v1/ticker/?limit=1800")
     { //faz a soma do valor em btc dos portfolios, para o total
         var tot_btc_geral = 0; //valor em btc somando todos os users
         var tot_real_geral = 0; //valor em reais somando todos os users
